de8181326d751b405d0ee327de0b3c62224ba18e
Adam Johnson <adamjohnson@apple.com>
2021-09-21 16:36:21 +1000

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -99,7 +99,7 @@ async function projectsLoad() {
         // Show overlay
         if (! window.projectID) {
             setTimeout(() => {
-                document.querySelector('#project').style.top = '120px'; // Open
+                document.querySelector('#project').style.top = '20%'; // Open
             }, 100); // Wait for display change
         }
 
