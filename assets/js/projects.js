facfd5738b353d0257fafac7c2a5e4ed65eff4be
Adam Johnson <adamjohnson@apple.com>
2021-09-21 15:22:57 +1000

    Revert

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -99,7 +99,7 @@ async function projectsLoad() {
         // Show overlay
         if (! window.projectID) {
             setTimeout(() => {
-                document.querySelector('#project').style.bottom = '120px'; // Open
+                document.querySelector('#project').style.top = '120px'; // Open
             }, 100); // Wait for display change
         }
 
